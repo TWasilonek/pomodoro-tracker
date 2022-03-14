a9a82779ccf9859213ef16d586ae647cd253e46c
TWasilonek <tomasz.wasilonek@gmail.com>
2022-03-14 15:32:23 +0100

    Removed Designs and the react native app

--- PomodoroTrackerReactNativeApp/index.js
+++
@@ -1,9 +0,0 @@
-import {AppRegistry} from 'react-native';
-
-//https://github.com/uuidjs/uuid#react-native--expo
-import 'react-native-get-random-values';
-
-import App from './src/App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
--- PomodoroTrackerReactNativeApp/src/constants/defaults.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_TASK_TIME = 25;
-export const DEFAULT_BREAK_TIME = 5;
